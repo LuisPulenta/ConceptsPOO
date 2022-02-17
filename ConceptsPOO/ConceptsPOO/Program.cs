4618d7d71a671206b13d3d7837421d643833b873
LuisPulenta <luisalbertonu@gmail.com>
2022-02-17 14:42:22 -0300

    Clase 2 Employee & SalaryEmployee & CommissionEmployee

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -9,18 +9,32 @@ namespace ConceptsPOO
             Console.WriteLine("POO COncepts");
             Console.WriteLine("============");
 
-            try
+            Employee employee1 = new SalaryEmployee()
             {
-                Console.WriteLine(new Date(2024, 2, 29));
-                Console.WriteLine(new Date(2022, 11, 30));
-                Console.WriteLine(new Date(1965, 6, 23));
-                Console.WriteLine(new Date(1985, 11, 5));
-                
-            }
-            catch (Exception error)
+                Id=1010,
+                Active = true,
+                BirthDate = new Date(1990,5,23),
+                FirstName ="Sandra",
+                LastName ="Morales",
+                HiringDate = new Date(2022, 1, 15),
+                Salary=1863453.45M
+            };
+
+            Console.WriteLine(employee1);
+
+            Employee employee2 = new CommissionEmployee
             {
-                Console.WriteLine(error.Message);
-            }
+                Id = 2020,
+                Active = true,
+                BirthDate = new Date(1995, 5, 23),
+                FirstName = "Patricia",
+                LastName = "Gutierrez",
+                HiringDate = new Date(2022, 2, 5),
+                Sales = 320000000M,
+                CommissionPercentaje =0.03F
+            };
+
+            Console.WriteLine(employee2);
         }
     }
 }
---
+++ ConceptsPOO/ConceptsPOO/SalaryEmployee.cs
@@ -0,0 +1,18 @@
+﻿namespace ConceptsPOO
+{
+    public class SalaryEmployee: Employee
+    {
+        public decimal Salary { get; set; }
+
+        public override decimal GetValueToPay()
+        {
+            return Salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" + 
+                $"\n\t Value to pay: {$"{GetValueToPay():C2}",18}";
+        }
+    }
+}
